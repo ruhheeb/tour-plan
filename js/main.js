ecf2753d77ac4f72294812d15a3f84031af6d794
ruhheeb <ruhheebmusic@gmail.com>
2020-07-21 18:52:06 +0400

    fix: edited map in hotel section

--- js/main.js
+++ js/main.js
@@ -14,3 +14,28 @@ var mySwiper = new Swiper('.swiper-container', {
   // effect 
   effect: "fade"
 });
+
+// Yandex Map
+ymaps.ready(init);
+
+function init() {
+  var myMap = new ymaps.Map('map', {
+      center: [55.833065, 37.484954],
+      zoom: 18
+    }, {
+      searchControlProvider: 'yandex#search'
+    }),
+    myPlacemark = new ymaps.Placemark(myMap.getCenter());
+
+  myMap.geoObjects.add(myPlacemark);
+
+  myPlacemark.events
+    .add('mouseenter', function (e) {
+      // Ссылку на объект, вызвавший событие,
+      // можно получить из поля 'target'.
+      e.get('target').options.set('preset', 'islands#greenIcon');
+    })
+    .add('mouseleave', function (e) {
+      e.get('target').options.unset('preset');
+    });
+}
