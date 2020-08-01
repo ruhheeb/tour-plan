1d3f278e55b69f47efc3ec9003be720e07e02f55
ruhheeb <ruhheebmusic@gmail.com>
2020-08-01 16:58:27 +0400

    create: modal for booking

--- js/main.js
+++ js/main.js
@@ -1,66 +1,90 @@
-// Hotel-slider
-var hotelSlider = new Swiper('.hotel-slider', {
-  // Optional parameters
-  loop: true,
-  // keyboard control
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-  // Navigation arrows
-  navigation: {
-    nextEl: '.hotel-slider__button--next',
-    prevEl: '.hotel-slider__button--prev',
-  },
-  // effect 
-  effect: "fade"
-});
+$(document).ready(function () {
+  // Hotel-slider
+  var hotelSlider = new Swiper('.hotel-slider', {
+    // Optional parameters
+    loop: true,
+    // keyboard control
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.hotel-slider__button--next',
+      prevEl: '.hotel-slider__button--prev',
+    },
+    // effect 
+    effect: "fade"
+  });
 
-var hotelSlider = new Swiper('.reviews-slider', {
-  // Optional parameters
-  loop: true,
-  // keyboard control
-  keyboard: {
-    enabled: true,
-    onlyInViewport: false,
-  },
-  // Navigation arrows
-  navigation: {
-    nextEl: '.reviews-slider__button--next',
-    prevEl: '.reviews-slider__button--prev',
-  },
+  var hotelSlider = new Swiper('.reviews-slider', {
+    // Optional parameters
+    loop: true,
+    // keyboard control
+    keyboard: {
+      enabled: true,
+      onlyInViewport: false,
+    },
+    // Navigation arrows
+    navigation: {
+      nextEl: '.reviews-slider__button--next',
+      prevEl: '.reviews-slider__button--prev',
+    },
 
-});
-
-
-// Yandex Map Javascript API
-ymaps.ready(init);
+  });
 
 
-function init() {
-  var myMap = new ymaps.Map('map', {
-      center: [55.833065, 37.484954],
-      zoom: 18
-    }, {
-      searchControlProvider: 'yandex#search'
-    }),
-    myPlacemark = new ymaps.Placemark(myMap.getCenter());
+  // Yandex Map Javascript API
+  ymaps.ready(init);
 
-  myMap.geoObjects.add(myPlacemark);
 
-  myPlacemark.events
-    .add('mouseenter', function (e) {
-      // Ссылку на объект, вызвавший событие,
-      // можно получить из поля 'target'.
-      e.get('target').options.set('preset', 'islands#greenIcon');
-    })
-    .add('mouseleave', function (e) {
-      e.get('target').options.unset('preset');
-    });
-}
+  function init() {
+    var myMap = new ymaps.Map('map', {
+        center: [55.833065, 37.484954],
+        zoom: 18
+      }, {
+        searchControlProvider: 'yandex#search'
+      }),
+      myPlacemark = new ymaps.Placemark(myMap.getCenter());
 
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener('click', function () {
-  console.log('Клик по кнопе меню');
-  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+    myMap.geoObjects.add(myPlacemark);
+
+    myPlacemark.events
+      .add('mouseenter', function (e) {
+        // Ссылку на объект, вызвавший событие,
+        // можно получить из поля 'target'.
+        e.get('target').options.set('preset', 'islands#greenIcon');
+      })
+      .add('mouseleave', function (e) {
+        e.get('target').options.unset('preset');
+      });
+  }
+
+  var menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener('click', function () {
+    console.log('Клик по кнопе меню');
+    document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible");
+  });
+
+
+  var modalButton = $('[data-toggle="modal"]');
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal)
+
+  function openModal() {
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.addClass("modal__overlay--visible");
+    modalDialog.addClass("modal__dialog--visible");
+  }
+
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $('.modal__overlay');
+    var modalDialog = $('.modal__dialog');
+    modalOverlay.removeClass("modal__overlay--visible");
+    modalDialog.removeClass("modal__dialog--visible");
+  }
+
 });
